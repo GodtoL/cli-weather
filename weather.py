1a29cbb200a34c2e87643a1a43ebfdbf4a5c9168
GodtoL <eliasvera793@gmail.com>
2024-08-27 15:54:09 -0400

    Prueba de commit

--- weather.py
+++ weather.py
@@ -47,4 +47,4 @@ elif format_data == 'csv':
 
     print("csv generado con éxito")
 else :
-    print(f"Error{data['cod']}. Algo ha salido muy mal")
+    print(f"Error{data['cod']}. Algo ha salido mal")
